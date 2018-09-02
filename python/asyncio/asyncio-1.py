240de34b40cd905c8d44623680f0c27ec40d41aa
Alexandre PECCAUD <alexandre.peccaud@gmail.com>
2018-09-02 12:40:35 +0800

    Update asyncio-1.py

--- python/asyncio/asyncio-1.py
+++ python/asyncio/asyncio-1.py
@@ -14,7 +14,6 @@ async def main():
     async with aiohttp.ClientSession() as session:
         futures = [fetch(session) for _ in range(10)]
         for i, future in enumerate(asyncio.as_completed(futures)):
-            # Now we can process the API response as soon as it comes
             result = await future
             print(f'{i} fetched {len(result)} results')
 
